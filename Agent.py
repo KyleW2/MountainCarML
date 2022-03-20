8fa28f4452e622588c3d1ef83c10dc8ed568a865
Kyle <Kyle.J.Weidner-1@ou.edu>
2022-03-19 22:03:23 -0500

    Corrected TDLambda algorithm

--- Agent.py
+++ Agent.py
@@ -1,10 +1,10 @@
 from TDLambda import TDLambda
 
 if __name__ == "__main__":
-    agent = TDLambda(0.6, 0.05, 0.9, 0.5, pickle = True, pickleFile = "TD_policy.pickle", load = True)
+    agent = TDLambda(0.6, 0.05, 0.9, 0.5, render = False, pickle = True, pickleFile = "TD_policy.pickle", load = True)
 
     try:
-        agent.runSeries(100000, render = False)
+        agent.runSeries(100000)
         agent.close()
     except KeyboardInterrupt:
         agent.savePolicy()
--- TDLambda.py
+++ TDLambda.py
@@ -6,13 +6,15 @@ from State import State
 from Step import Step
 
 class TDLambda:
-    def __init__(self, lam: float, alpha: float, gamma: float, epsilon: float, pickle = False, pickleFile = None, load = False):
+    def __init__(self, lam: float, alpha: float, gamma: float, epsilon: float, render = False, pickle = False, pickleFile = None, load = False):
         self.env = gym.make("MountainCar-v0")
         
         self.lam = lam
         self.alpha = alpha
         self.gamma = gamma
         self.epsilon = epsilon
+
+        self.render = render
 
         # Keys: tuple = (pos, vel)
         # Value: State = State()
@@ -38,101 +40,106 @@ class TDLambda:
         self.episode = []
 
         self.wins = 0
+        self.highestPoint = 0
 
     def resetElig(self) -> None:
         for v in self.policy.values():
             v.resetElig()
     
-    def getNextAction(self, state: tuple) -> int:
-        # If we've seen the state then get the action for highest value state 
-        if state in self.policy.keys():
-            # Chance to explore or act greedy
-            if random.random() < (1 - self.epsilon):
-                possibles =  self.policy[state].getNextStates()
+    def eGreedy(self, state: tuple) -> int:
+        # Chance to explore or act greedy
+        if random.random() < (1 - self.epsilon):
+            possibles =  self.policy[state].getNextStates()
 
-                if len(possibles) == 0:
-                    return random.choice([0, 1, 2])
-
-                bestAction = 0
-                bestValue = -10000
-
-                for k, v in possibles.items():
-                    if self.policy[v].getValue() > bestValue:
-                        bestValue = self.policy[v].getValue()
-                        bestAction = k
-                
-                return bestAction
-            else:
+            # If possibles are empty then random move
+            if len(possibles) == 0:
                 return random.choice([0, 1, 2])
-        # Otherwise add state to the policy and return a random move
+
+            bestAction = 0
+            bestValue = -10000
+
+            for k, v in possibles.items():
+                if self.policy[v].getValue() > bestValue:
+                    bestValue = self.policy[v].getValue()
+                    bestAction = k
+            
+            return bestAction
         else:
-            self.policy[state] = State(state[0], state[1])
             return random.choice([0, 1, 2])
     
-    def runEpisode(self, render = False) -> None:
+    def runEpisode(self) -> None:
+        # Reset stuff that needs to be reset
         self.episode = []
         self.resetElig()
-
         observation = self.env.reset()
         done = False
-        eps = 0
+        step = 0
+        highest = -1.2
 
+        # Loop for each step
         while not done:
-            if render: 
+            # Toggle rendering
+            if self.render: 
                 self.env.render()
 
+            # Create tuple representing current state
             pos = round(observation[0], 2)
             vel = round(observation[1], 2)
             stateTuple = (pos, vel)
 
-            action = self.getNextAction(stateTuple)
+            # Add state to policy if new
+            if stateTuple not in self.policy.keys():
+                self.policy[stateTuple] = State(pos, vel)
+
+            # Find next action using e-greedy policy
+            action = self.eGreedy(stateTuple)
+
+            # Take action a and observe r, s'
             observation, reward, done, info = self.env.step(action)
 
+            # Update state with a and s'
+            newState = (round(observation[0], 2), round(observation[1], 2))
+            self.updateNextStates(stateTuple, action, newState)
+
+            # Add s' to policy if new
+            if newState not in self.policy.keys():
+                self.policy[newState] = State(pos, vel)
+
+            # Add Step object to episode
             self.episode.append(Step(stateTuple, action, reward))
 
+            # Update values and eligiblity traces for all s
+            self.updateStateValues(reward, newState, stateTuple)
+
+            # Metrics
             if reward == 0:
                 self.wins += 1
-
-            eps += 1
-        
-        self.policyEval()
-        self.updateNextStates()
-        #self.printValues()
+            if observation[0] > highest:
+                highest = observation[0]
+            self.highestPoint = highest
+            step += 1
     
-    def printEpisode(self) -> None:
-        print("Episode length: " + str(len(self.episode)))
+    def updateStateValues(self, reward, newState, currentState) -> None:
+        # Calculate delta
+        delta = reward
+        delta += (self.gamma * self.policy[newState].getValue()) 
+        delta -= self.policy[currentState].getValue()
 
-        for i in range(0, len(self.episode)):
-            print(self.episode[i])
-    
-    def policyEval(self) -> None:
-        for i in range(0, len(self.episode) - 1):
-            delta = self.episode[i].getReward()
-            delta += (self.gamma * self.policy[self.episode[i+1].getState()].getValue()) 
-            delta -= self.policy[self.episode[i].getState()].getValue()
-            
-            self.policy[self.episode[i].getState()].updateElig(self.gamma, self.lam, True)
+        # Update eligiblity trace for current state
+        self.policy[currentState].updateElig(self.gamma, self.lam, True)
 
-            for v in self.policy.values():
-                v.updateValuePreDelta(self.alpha, delta)
-                v.updateElig(self.gamma, self.lam, False)
+        # Loop thru all states and update
+        for s in self.policy.values():
+            s.updateValuePreDelta(self.alpha, delta)
+            s.updateElig(self.gamma, self.lam, False)
     
-    def updateNextStates(self) -> None:
-        for i in range(0, len(self.episode) - 1):
-            self.policy[self.episode[i].getState()].addNextState(self.episode[i].getAction(), self.episode[i+1].getState())
-    
-    def highestPoint(self) -> float:
-        max = -1.0
-        for i in range(0, len(self.episode)):
-            if self.episode[i].getState()[0] > max:
-                max = self.episode[i].getState()[0]
-        
-        return round(max, 4)
+    def updateNextStates(self, stateTuple: tuple, action: int, nextState: tuple) -> None:
+        self.policy[stateTuple].addNextState(action, nextState)
 
-    def runSeries(self, episodes: int, render = False) -> None:
+    def runSeries(self, episodes: int) -> None:
         for i in range(0, episodes):
-            self.runEpisode(render)
-            print(f"Episode {i}, visited {len(self.policy.keys())} states, total wins are {self.wins}, highest point was {self.highestPoint()}")
+            self.runEpisode()
+            print(f"episode: {i}, visited: {len(self.policy.keys())}, wins: {self.wins}, highest: {self.highestPoint}")
         
         self.savePolicy()
     
