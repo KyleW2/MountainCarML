91670b2cf63220224429e45643192417cb38aeb1
Tyler Sanbar <tylersanbar@gmail.com>
2022-03-22 19:48:20 -0500

    Added RichSutton's for comparison

--- Agent.py
+++ Agent.py
@@ -2,10 +2,10 @@ from TDLambda import TDLambda
 from Sarsa import Sarsa
 
 if __name__ == "__main__":
-    agent = Sarsa(0.5, 0.5, 0.0, render = False, pickle = True, pickleFile = "Sarsa_policy.pickle", load = True)
+    agent = Sarsa(0.05, 0.7, 0.0, render = False, pickle = True, pickleFile = "Sarsa_policy.pickle", load = True)
 
     try:
-        agent.runSeries(100000)
+        agent.runSeries(1000000)
         agent.close()
     except KeyboardInterrupt:
         agent.savePolicy()
