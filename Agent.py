38148bbda227036a7c7e4f6bed23714db94ad6a7
Tyler Sanbar <tylersanbar@gmail.com>
2022-03-27 13:42:23 -0500

    Added plotting for TD State policy

--- Agent.py
+++ Agent.py
@@ -2,12 +2,15 @@ from TDLambda import TDLambda
 from Sarsa import Sarsa
 
 if __name__ == "__main__":
-    #agent = Sarsa(0.05, 0.7, 0.0, render = False, pickle = True, pickleFile = "Sarsa_policy.pickle", load = True)
-    agent = TDLambda(lam = 0.7, alpha = 0.05, epsilon = 1.0, gamma = .5,
+    agentSarsa = Sarsa(0.05, 0.7, 0.0, render = False, pickle = True, pickleFile = "Sarsa_policy.pickle", load = False)
+    agentTD = TDLambda(lam = 0.7, alpha = 0.05, epsilon = 1.0, gamma = .5,
                      render = False, pickle = True, pickleFile = "TD_policy.pickle", load = False)
     try:
-        agent.runSeries(1000000)
-        agent.close()
+        agentTD.runSeries(10000)
+        agentTD.close()
+        agentSarsa.runSeries(10000)
+        agentSarsa.close()
     except KeyboardInterrupt:
-        agent.savePolicy()
+        agentTD.savePolicy()
+        agentSarsa.savePolicy()
         print("Closed!")
--- plot.py
+++ plot.py
@@ -2,25 +2,59 @@ import pickle
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import matplotlib.pyplot as plt
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-f = open('Sarsa_policy.pickle', "rb")
-policy = pickle.load(f)
 
-x = []
-y = []
-z = []
+def plotActionPolicy(filename):
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    f = open(filename, "rb")
+    policy = pickle.load(f)
 
-def f(x,y):
-    t = (x, y)
-    return policy[t].actionValues[policy[t].getMaxAction()]
+    x = []
+    y = []
+    z = []
 
-for state in policy.values():
-    x.append(state.position)
-    y.append(state.velocity)
-    z.append(-state.actionValues[state.getMaxAction()])
+    def f(x,y):
+        t = (x, y)
+        return policy[t].actionValues[policy[t].getMaxAction()]
 
-#X, Y = np.meshgrid(x, y)
-#Z = f(x,y)
-ax.scatter(x, y, z)
-plt.show()
+    for state in policy.values():
+        x.append(state.position)
+        y.append(state.velocity)
+        z.append(-state.actionValues[state.getMaxAction()])
+
+    #X, Y = np.meshgrid(x, y)
+    #Z = f(x,y)
+    ax.scatter(x, y, z)
+    plt.show()
+
+def plotStatePolicy(filename):
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    f = open(filename, "rb")
+    policy = pickle.load(f)
+
+    x = []
+    y = []
+    z = []
+
+    def f(x,y):
+        t = (x, y)
+        return policy[t].actionValues[policy[t].getMaxAction()]
+
+    for state in policy.values():
+        x.append(state.position)
+        y.append(state.velocity)
+        z.append(-state.getValue())
+
+    #X, Y = np.meshgrid(x, y)
+    #Z = f(x,y)
+    ax.scatter(x, y, z)
+    plt.show()
+
+sarsaFile = 'Sarsa_policy.pickle'
+tdFile = 'TD_policy.pickle'
+
+plotActionPolicy(sarsaFile)
+plotStatePolicy(tdFile)
