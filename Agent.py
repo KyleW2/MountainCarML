d8d0943ce6334a2cd7263b5d2e16a77c996af231
Kyle <Kyle.J.Weidner-1@ou.edu>
2022-03-28 18:18:57 -0500

    Testing Q Learning

--- Agent.py
+++ Agent.py
@@ -1,16 +1,14 @@
 from TDLambda import TDLambda
 from Sarsa import Sarsa
+from QLearning import QLearning
 
 if __name__ == "__main__":
     agentSarsa = Sarsa(0.05, 0.7, 0.0, render = False, pickle = True, pickleFile = "Sarsa_policy.pickle", load = False)
-    agentTD = TDLambda(lam = 0.7, alpha = 0.05, epsilon = 1.0, gamma = .5,
+    agentTD = TDLambda(lam = 0, alpha = 0.05, epsilon = 0.0, gamma = 0.99,
                      render = False, pickle = True, pickleFile = "TD_policy.pickle", load = False)
+    agentQ = QLearning(0.05, 0.7, 1.0, render = False, pickle = True, pickleFile = "Q_policy.pickle", load = False)
     try:
-        agentTD.runSeries(10000)
-        agentTD.close()
-        agentSarsa.runSeries(10000)
-        agentSarsa.close()
+        agentQ.runSeries(100000)
     except KeyboardInterrupt:
-        agentTD.savePolicy()
-        agentSarsa.savePolicy()
+        agentQ.savePolicy()
         print("Closed!")
