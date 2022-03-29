278641a158ab86e5b934509de902bed613f3168d
Kyle <Kyle.J.Weidner-1@ou.edu>
2022-03-28 21:00:11 -0500

    Implemented Q function for MC

---
+++ MCQState.py
@@ -0,0 +1,42 @@
+import random
+
+class MCQState:
+    def __init__(self, position: float, velocity: float) -> None:
+        self.position = position
+        self.velocity = velocity
+
+        self.returns = {0: [], 1: [], 2: []}
+        self.sumOfReturns = {0: 0, 1: 0, 2: 0}
+
+        self.actionValues = {0: 0, 1: 0, 2: 0}
+    
+    def __str__(self) -> str:
+        return "(" + str(self.position) + ", " + str(self.velocity) + ")"
+    
+    def getMaxAction(self) -> int:
+        max = -100000000
+        action = -1
+
+        for k, v in self.actionValues.items():
+            if v > max:
+                max = v
+                action = k
+        
+        return action
+    
+    def updateReturns(self, action: int, reward: float) -> None:
+        self.returns[action].append(reward)
+        # Keep a running sum of the returns of avoid a for loop later
+        self.sumOfReturns[action] += reward
+    
+    def getActionValue(self, action: int) -> float:
+        return self.actionValues[action]
+    
+    def updateValue(self, action: int, alpha: float) -> None:
+        '''
+        # NOT a += just =
+        self.value = self.sumOfReturns / len(self.returns)
+        '''
+        # Chaning to profs update funtion
+        g = self.sumOfReturns[action] / len(self.returns[action])
+        self.actionValues[action] += alpha * (g - self.actionValues[action])
---
+++ MonteCarloQ.py
@@ -0,0 +1,144 @@
+import gym
+import random
+import pickle as pickler
+
+from MCQState import MCQState
+from Step import Step
+
+class MonteCarloQ:
+    def __init__(self, alpha: float, gamma: float, epsilon: float, render = False, pickle = False, pickleFile = None, load = False) -> None:
+        self.env = gym.make("MountainCar-v0")
+        
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
+
+        # Keys: tuple = (pos, vel)
+        # Value: State = State()
+        self.policy = {}
+
+        self.render = render
+        self.pickle = pickle
+
+        # Pickle stuff
+        if self.pickle:
+            self.pickleFile = pickleFile
+
+            try:
+                f = open(self.pickleFile, "rb")
+            except FileNotFoundError:
+                f = open(self.pickleFile, "w")
+                f.close()
+                f = open(self.pickleFile, "rb")
+
+            if load:
+                self.policy = pickler.load(f)
+            f.close()
+
+        # Class var for induvidual episodes
+        # Reset each time
+        self.episode = []
+
+        # Metrics
+        self.wins = 0
+        self.highestPoint = 0
+    
+    def eGreey(self, state: tuple):
+        # Chance to be greedy
+        if random.random() < (1 - self.epsilon):
+            # Return max action
+            return self.policy[state].getMaxAction()
+        # Explore with random action
+        else:
+            return random.choice([0, 1, 2])
+    
+    def updateV(self):
+        # Nest for loops uh-oh!
+        # RIP and sort of speed
+        alreadyVisited = []
+        for i in range(0, len(self.episode)):
+            stateTuple = self.episode[i].getState()
+            action = self.episode[i].getAction()
+
+            # If first occurence of s
+            if (stateTuple, action) not in alreadyVisited:
+                # Sum rewards
+                r = 0
+                for j in range(i, len(self.episode)):
+                    r += self.episode[j].getReward()
+
+                # Append r to s's returns
+                self.policy[stateTuple].updateReturns(action, r)
+                self.policy[stateTuple].updateValue(action, self.alpha)
+
+                # Added to list of visited states
+                alreadyVisited.append((stateTuple, action))
+    
+    def runEpisode(self) -> None:
+        # Reset the stuff
+        self.episode = []
+        observation = self.env.reset()
+        done = False
+        steps = 0
+        highest = -1.2
+
+        # Loop for each step
+        while not done:
+            # Toggle rendering
+            if self.render: 
+                self.env.render()
+
+            # Create tuple representing current state
+            pos = round(observation[0], 1)
+            vel = round(observation[1], 2)
+            stateTuple = (pos, vel)
+
+            # Add state to policy if new
+            if stateTuple not in self.policy.keys():
+                self.policy[stateTuple] = MCQState(pos, vel)
+            
+            # Find next action using e-greedy
+            action = self.eGreey(stateTuple)
+
+            # Observe r and s'
+            observation, reward, done, info = self.env.step(action)
+
+            # Ensure reward is correct
+            if observation[0] >= 0.5:
+                reward = 0
+            else:
+                reward = -1
+
+            # Add step to episode
+            self.episode.append(Step(stateTuple, action, reward))
+
+            # Metrics
+            if reward == 0 and done:
+                self.wins += 1
+            if observation[0] > highest:
+                highest = observation[0]
+            self.highestPoint = highest
+            steps += 1
+        
+        self.updateV()
+
+    def runSeries(self, episodes: int) -> None:
+        for i in range(0, episodes):
+            self.runEpisode()
+            print(f"episode: {i}, visited: {len(self.policy.keys())}, wins: {self.wins}, win rate: {self.wins/(i+1)}, epsilon: {self.epsilon}, highest: {self.highestPoint}")
+            
+            if i > 5000:
+                self.epsilon *= 0.999
+            if i > 12000:
+                self.epsilon = 0.0
+        
+        self.savePolicy()
+    
+    def savePolicy(self) -> None:
+        if self.pickle:
+            f = open(self.pickleFile, "wb")
+            pickler.dump(self.policy, f)
+            f.close()
+    
+    def close(self):
+        self.env.close()
