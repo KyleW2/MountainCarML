5fabdba02cf5d2cc5bafc10636eb7fd6620a2beb
Tyler Sanbar <tylersanbar@gmail.com>
2022-03-30 13:36:24 -0500

    Everything ready to go

--- QLearning.py
+++ QLearning.py
@@ -20,7 +20,7 @@ class QLearning:
 
         self.render = render
         self.pickle = pickle
-
+        self.rewards = []
         # Pickle stuff
         if self.pickle:
             self.pickleFile = pickleFile
@@ -43,7 +43,7 @@ class QLearning:
         # Metrics
         self.wins = 0
         self.highestPoint = 0
-    
+
     def eGreey(self, state: tuple):
         # Chance to be greedy
         if random.random() < (1 - self.epsilon):
@@ -119,21 +119,15 @@ class QLearning:
             self.highestPoint = highest
             steps += 1
             cumulative_reward += reward
-        return cumulative_reward
+        self.rewards.append(cumulative_reward)
+
     def runSeries(self, episodes: int) -> None:
-        rewards = list()
         for i in range(0, episodes):
             self.runEpisode()
-            rewards.append(self.runEpisode())
-
             print(f"episode: {i}, visited: {len(self.policy.keys())}, wins: {self.wins}, win rate: {self.wins/(i+1)}, epsilon: {self.epsilon}")
-            if self.epsilon > .1: self.epsilon = .9999*self.epsilon
+            if self.epsilon > .1: self.epsilon *= .9999
         self.savePolicy()
-        QLearning.plot_curve(rewards, filepath="./Qreward.png",
-            x_label="Episode", y_label="Reward",
-            x_range=(0, len(rewards)), y_range=(-3.1,0.1),
-            color="red", kernel_size=500,
-            alpha=0.4, grid=True)
+
     
     def savePolicy(self) -> None:
         if self.pickle:
@@ -143,31 +137,3 @@ class QLearning:
     
     def close(self):
         self.env.close()
-
-    def plot_curve(data_list, filepath="./my_plot.png",
-               x_label="X", y_label="Y",
-               x_range=(0, 1), y_range=(0,1), color="-r", kernel_size=50, alpha=0.4, grid=True):
-        """Plot a graph using matplotlib
-
-        """
-        if(len(data_list) <=1):
-            print("[WARNING] the data list is empty, no plot will be saved.")
-            return
-        fig = plt.figure()
-        ax = fig.add_subplot(111, autoscale_on=True)
-        ax.grid(grid)
-        ax.set_xlabel(x_label)
-        ax.set_ylabel(y_label)
-        ax.plot(data_list, color, alpha=alpha)  # The original data is showed in background
-        kernel = np.ones(int(kernel_size))/float(kernel_size)  # Smooth the graph using a convolution
-        tot_data = len(data_list)
-        lower_boundary = int(kernel_size/2.0)
-        upper_boundary = int(tot_data-(kernel_size/2.0))
-        data_convolved_array = np.convolve(data_list, kernel, 'same')[lower_boundary:upper_boundary]
-        #print("arange: " + str(np.arange(tot_data)[lower_boundary:upper_boundary]))
-        #print("Convolved: " + str(np.arange(tot_data).shape))
-        ax.plot(np.arange(tot_data)[lower_boundary:upper_boundary], data_convolved_array, color, alpha=1.0)  # Convolved plot
-        fig.savefig(filepath)
-        fig.clear()
-        plt.close(fig)
-        # print(plt.get_fignums())  # print the number of figures opened in background
