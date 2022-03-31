b786981d74614cba87e3f5d75330c549848a610b
Tyler Sanbar <tylersanbar@gmail.com>
2022-03-31 17:35:11 -0500

    Updated graphs

--- Agent.py
+++ Agent.py
@@ -1,3 +1,4 @@
+from math import inf
 from MonteCarloQ import MonteCarloQ
 from TDLambda import TDLambda
 from Sarsa import Sarsa
@@ -34,10 +35,10 @@ def runMCQ(alphas, gamma, epsilon, iters):
             agents.append(MonteCarloQ(alphas[i], 10, epsilon, False, None, False))
             agents[i].runSeries(iters)
             rewardLists.append(agents[i].rewards)
-        saveResults((alphas, rewardLists), "FinalQResults.pickle")
+        saveResults((alphas, rewardLists), "FinalMCQResults.pickle")
         return rewardLists
     except KeyboardInterrupt:
-        saveResults((alphas,rewardLists), "FinalQResultsInterupt.pickle")
+        saveResults((alphas,rewardLists), "FinalMCQResultsInterupt.pickle")
         print("Closed!")
 
 def runQ(alphas, gamma, epsilon, iters):
@@ -48,16 +49,207 @@ def runQ(alphas, gamma, epsilon, iters):
             agents.append(QLearning(alphas[i], gamma, epsilon, False, None, False))
             agents[i].runSeries(iters)
             rewardLists.append(agents[i].rewards)
-        saveResults((alphas, rewardLists), "FinalMCQResults.pickle")
+        saveResults((alphas, rewardLists), "FinalQResults.pickle")
         return rewardLists
     except KeyboardInterrupt:
-        saveResults((alphas,rewardLists), "FinalMCQResultsInterupt.pickle")
+        saveResults((alphas,rewardLists), "FinalQResultsInterupt.pickle")
         print("Closed!")
 
-def plotRewards(rewardLists, filepath):
+def plotRewards(rewardLists, alphas, filepath):
     plot.plot_curves(rewardLists, alphas, filepath=filepath, x_label="Episode", y_label="Reward", color="red", kernel_size=500, grid=True)
 
-if __name__ == "__main__":
+def plotHighestRewards(rewardLists, names, filepath):
+    plot.plot_highest_curves(rewardLists, names, filepath=filepath, x_label="Episode", y_label="Reward", kernel_size=500, grid=True)
+
+def plotHighest():
+    sarsaFile = "FinalSarsaResults.pickle"
+    mcqFile = "FinalMCQResults.pickle"
+    qFile = "FinalQResults.pickle"
+    
+    try:
+        fSarsa = open(sarsaFile, "rb")
+        fMcq = open(mcqFile, "rb")
+        fQ = open(qFile, "rb")
+        sarsaRewards = pickler.load(fSarsa)
+        mcqRewards = pickler.load(fMcq)
+        qRewards = pickler.load(fQ)
+    except FileNotFoundError:
+        fSarsa.close()
+        fMcq.close()
+        fQ.close()
+
+    #plotRewards(mcqRewards[1], mcqRewards[0], "test.png")
+
+    highestSarsa = highestReward(sarsaRewards)
+    highestMcq = highestReward(mcqRewards)
+    highestQ = highestReward(qRewards)
+    rewardLists = [highestSarsa, highestMcq, highestQ]
+    names = ["Sarsa", "MCQ", "Q"]
+    plotHighestRewards(rewardLists, names, "highest.png")
+
+    fSarsa.close()
+    fMcq.close()
+    fQ.close()
+
+def getWins(rewards):
+    wins = []
+    for reward in rewards:
+        if reward > -200: wins.append(reward)
+    return wins
+
+def bestScore():
+    sarsaFile = "FinalSarsaResults.pickle"
+    mcqFile = "FinalMCQResults.pickle"
+    qFile = "FinalQResults.pickle"
+    
+    try:
+        fSarsa = open(sarsaFile, "rb")
+        fMcq = open(mcqFile, "rb")
+        fQ = open(qFile, "rb")
+        sarsaRewards = pickler.load(fSarsa)
+        mcqRewards = pickler.load(fMcq)
+        qRewards = pickler.load(fQ)
+    except FileNotFoundError:
+        fSarsa.close()
+        fMcq.close()
+        fQ.close()
+
+    bestScore = -200
+    a = sarsaRewards[1]
+    b = mcqRewards[1]
+    c = qRewards[1]
+    for alpha in a:
+        for reward in alpha:
+            if reward > bestScore: bestScore = reward
+    for alpha in b:
+        for reward in alpha:
+            if reward > bestScore: bestScore = reward
+    for alpha in c:
+        for reward in alpha:
+            if reward > bestScore: bestScore = reward
+
+    print("Best Score:", bestScore)
+
+def firstWin():
+    sarsaFile = "FinalSarsaResults.pickle"
+    mcqFile = "FinalMCQResults.pickle"
+    qFile = "FinalQResults.pickle"
+    
+    try:
+        fSarsa = open(sarsaFile, "rb")
+        fMcq = open(mcqFile, "rb")
+        fQ = open(qFile, "rb")
+        sarsaRewards = pickler.load(fSarsa)
+        mcqRewards = pickler.load(fMcq)
+        qRewards = pickler.load(fQ)
+    except FileNotFoundError:
+        fSarsa.close()
+        fMcq.close()
+        fQ.close()
+
+    firstSarsa = 100000
+    alphaSarsa = None
+    firstMCQ = 100000
+    alphaMCQ = None
+    firstQ = 100000
+    alphaQ = None
+
+    print("First Sarsa Win")
+
+    for i in range(len(sarsaRewards[0])):
+        rewards = sarsaRewards[1][i]
+        alpha = sarsaRewards[0][i]
+        for episode in range(len(rewards)):
+            if rewards[episode] > -200:
+                if firstSarsa > episode: 
+                    firstSarsa = episode
+                    alphaSarsa = alpha
+                break
+    print("Episode:",firstSarsa)
+    print("Alpha:",alphaSarsa)
+
+    print("First MCQ Win")
+
+    for i in range(len(mcqRewards[0])):
+        rewards = mcqRewards[1][i]
+        alpha = mcqRewards[0][i]
+        for episode in range(len(rewards)):
+            if rewards[episode] > -200:
+                if firstMCQ > episode: 
+                    firstMCQ= episode
+                    alphaMCQ = alpha
+                break
+    print("Episode:",firstMCQ)
+    print("Alpha:",alphaMCQ)
+
+    print("First Q Win")
+
+    for i in range(len(qRewards[0])):
+        rewards = qRewards[1][i]
+        alpha = qRewards[0][i]
+        for episode in range(len(rewards)):
+            if rewards[episode] > -200:
+                if firstQ > episode: 
+                    firstQ = episode
+                    alphaQ = alpha
+                break
+    print("Episode:",firstQ)
+    print("Alpha:",alphaQ)
+
+def plotWins():
+    sarsaFile = "FinalSarsaResults.pickle"
+    mcqFile = "FinalMCQResults.pickle"
+    qFile = "FinalQResults.pickle"
+    
+    try:
+        fSarsa = open(sarsaFile, "rb")
+        fMcq = open(mcqFile, "rb")
+        fQ = open(qFile, "rb")
+        sarsaRewards = pickler.load(fSarsa)
+        mcqRewards = pickler.load(fMcq)
+        qRewards = pickler.load(fQ)
+    except FileNotFoundError:
+        fSarsa.close()
+        fMcq.close()
+        fQ.close()
+
+    #plotRewards(mcqRewards[1], mcqRewards[0], "test.png")
+
+    highestSarsa = highestReward(sarsaRewards)
+    highestMcq = highestReward(mcqRewards)
+    highestQ = highestReward(qRewards)
+    winsSarsa = getWins(highestSarsa)
+    winsMcq = getWins(highestMcq)
+    winsQ = getWins(highestQ)
+    rewardLists = [winsSarsa, winsMcq, winsQ]
+    names = ["Sarsa", "MCQ", "Q"]
+    plotHighestRewards(rewardLists, names, "highestWins.png")
+
+    fSarsa.close()
+    fMcq.close()
+    fQ.close()
+
+def highestReward(rewardList):
+    highestSum = -inf
+    highestRewards = None
+    highestAlpha = None
+    for i in range( len(rewardList[1]) ):
+        rewards = rewardList[1][i]
+        alpha = rewardList[0][i]
+        sum = 0
+        for reward in rewards:
+            sum += reward
+        
+        if sum > highestSum:
+            highestSum = sum
+            highestRewards = rewards
+            highestAlpha = alpha
+
+    print("Sum:", sum)
+    print("Alpha:", highestAlpha)
+    return highestRewards
+
+def runExperiments():
     iters = 100000
     gamma = .999
     alphas = [.001, .01, .1, .5, .9]
@@ -65,6 +257,9 @@ if __name__ == "__main__":
     sarsaRewards = runSarsas(alphas, gamma, epsilon, iters)
     qRewards = runQ(alphas, gamma, epsilon, iters)
     mcqRewards = runMCQ(alphas, gamma, epsilon, iters)
-    plotRewards(sarsaRewards, "FinalSarsa.png")
-    plotRewards(qRewards, "FinalQ.png")
-    plotRewards(mcqRewards, "FinalMCQ.png")
+    plotRewards(sarsaRewards, alphas, "FinalSarsa.png")
+    plotRewards(qRewards, alphas, "FinalQ.png")
+    plotRewards(mcqRewards, alphas, "FinalMCQ.png")
+
+if __name__ == "__main__":
+    bestScore()
--- plot.py
+++ plot.py
@@ -74,6 +74,33 @@ def plot_curve(data_list, filepath="./my_plot.png",
     plt.close(fig)
     # print(plt.get_fignums())  # print the number of figures opened in background
 
+def plot_highest_curves(data_lists, names, filepath="./my_plot.png", x_label="X", y_label="Y", color="-r", kernel_size=50, grid=True):
+    """Plot a graph using matplotlib
+
+    """
+    if(len(data_lists[0]) <=1):
+        print("[WARNING] the data list is empty, no plot will be saved.")
+        return
+    fig = plt.figure()
+    ax = fig.add_subplot(111, autoscale_on=True)
+    ax.grid(grid)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    #ax.plot(data_list, color, alpha=alpha)  # The original data is showed in background
+    kernel = np.ones(int(kernel_size))/float(kernel_size)  # Smooth the graph using a convolution
+    for i in range(len(data_lists)):
+        data_list = data_lists[i]
+        tot_data = len(data_list)
+        lower_boundary = int(kernel_size/2.0)
+        upper_boundary = int(tot_data-(kernel_size/2.0))
+        data_convolved_array = np.convolve(data_list, kernel, 'same')[lower_boundary:upper_boundary]
+        #print("arange: " + str(np.arange(tot_data)[lower_boundary:upper_boundary]))
+        #print("Convolved: " + str(np.arange(tot_data).shape))
+        ax.plot(np.arange(tot_data)[lower_boundary:upper_boundary], data_convolved_array, label = "Algorithm: " + names[i], alpha=1.0)  # Convolved plot
+    ax.legend()
+    fig.savefig(filepath)
+    fig.clear()
+    plt.close(fig)
 
 def plot_curves(data_lists, alphas, filepath="./my_plot.png", x_label="X", y_label="Y", color="-r", kernel_size=50, grid=True):
     """Plot a graph using matplotlib
